f7ed2d3560dbeb8397a4f87e899bb84b42529969
dhgaj30-jpg <dhgaj30-jpg@example.com>
2026-06-28 20:00:18 +0300

    Fix KeyboardOptions import

--- app/src/main/java/com/example/prayertime/ui/Screens.kt
+++ app/src/main/java/com/example/prayertime/ui/Screens.kt
@@ -1,6 +1,6 @@
 package com.example.prayertime.ui
 
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardOptions
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
